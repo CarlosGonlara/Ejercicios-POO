dc7090a4f26ae87e1990b136f409ff685a7821ef
Carlos Gonlara <carlosglzlara22@gmail.com>
2026-03-13 21:39:43 -0700

    Minecraft

--- Ejercicio06/main.py
+++ Ejercicio06/main.py
@@ -3,12 +3,14 @@ from mob import Mob
 from creeper import Creeper    
 from enderman import Enderman
 from vaca import Vaca
-
+from zombie import Zombie
+ 
 if __name__ == "__main__":
     mobs = [
         Vaca("Bessie", 10),
         Creeper("Explosi", 20),
         Enderman("Tall Boi", 40),
+        Zombie ("Zombie", 15),
     ]
     for mob in mobs:
         mob.presentarse()
